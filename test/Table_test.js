4574a453d453368492fd1f9f81f6b390ccb44837
ajlopez <webmaster@ajlopez.com>
2020-11-21 18:03:36 -0300

    Tests refacto

--- test/Table_test.js
+++ test/Table_test.js
@@ -2,86 +2,84 @@
 Table = artifacts.require('Table');
 
 contract('Table', function (accounts) {
-    it('create contract', async function () {
-        const table = await Table.new(4);
+    describe('initial tests', function () {
+        let table;
         
-        const noColumns = Number(await table.noColumns());
-        const noRows = Number(await table.noRows());
+        beforeEach(async function () {
+            table = await Table.new(4);
+        });
         
-        assert.equal(noColumns, 4);
-        assert.equal(noRows, 0);
-    });
-    
-    it('add row', async function () {
-        const table = await Table.new(4);
+        it('create contract', async function () {          
+            const noColumns = Number(await table.noColumns());
+            const noRows = Number(await table.noRows());
+            
+            assert.equal(noColumns, 4);
+            assert.equal(noRows, 0);
+        });
         
-        await table.addRow(createRowData(0, 4));
+        it('add row', async function () {
+            await table.addRow(createRowData(0, 4));
+            
+            const noColumns = Number(await table.noColumns());
+            const noRows = Number(await table.noRows());
+            
+            assert.equal(noColumns, 4);
+            assert.equal(noRows, 1);
+        });
         
-        const noColumns = Number(await table.noColumns());
-        const noRows = Number(await table.noRows());
+        it('add and get row', async function () {
+            await table.addRow(createRowData(0, 4));
+            const row = await table.getRow(0);
+            
+            const noColumns = Number(await table.noColumns());
+            const noRows = Number(await table.noRows());
+            
+            assert.equal(noColumns, 4);
+            assert.equal(noRows, 1);
+            
+            checkRowData(row, 0, 4);
+        });
         
-        assert.equal(noColumns, 4);
-        assert.equal(noRows, 1);
-    });
-    
-    it('add and get row', async function () {
-        const table = await Table.new(4);
-        
-        await table.addRow(createRowData(0, 4));
-        const row = await table.getRow(0);
-        
-        const noColumns = Number(await table.noColumns());
-        const noRows = Number(await table.noRows());
-        
-        assert.equal(noColumns, 4);
-        assert.equal(noRows, 1);
-        
-        checkRowData(row, 0, 4);
-    });
-    
-    it('add and get two rows', async function () {
-        const table = await Table.new(4);
-        
-        await table.addRow(createRowData(0, 4));
-        await table.addRow(createRowData(1, 4));
+        it('add and get two rows', async function () {
+            await table.addRow(createRowData(0, 4));
+            await table.addRow(createRowData(1, 4));
 
-        const noColumns = Number(await table.noColumns());
-        const noRows = Number(await table.noRows());
+            const noColumns = Number(await table.noColumns());
+            const noRows = Number(await table.noRows());
+            
+            assert.equal(noColumns, 4);
+            assert.equal(noRows, 2);
+            
+            const row = await table.getRow(0);
+            
+            checkRowData(row, 0, 4);
+            
+            const row2 = await table.getRow(1);
+            
+            checkRowData(row2, 1, 4);
+        });
         
-        assert.equal(noColumns, 4);
-        assert.equal(noRows, 2);
-        
-        const row = await table.getRow(0);
-        
-        checkRowData(row, 0, 4);
-        
-        const row2 = await table.getRow(1);
-        
-        checkRowData(row2, 1, 4);
-    });
-    
-    it('add, update and get row', async function () {
-        const table = await Table.new(4);
-        
-        await table.addRow(createRowData(0, 4));
+        it('add, update and get row', async function () {
+            await table.addRow(createRowData(0, 4));
 
-        const noColumns = Number(await table.noColumns());
-        const noRows = Number(await table.noRows());
-        
-        assert.equal(noColumns, 4);
-        assert.equal(noRows, 1);
-        
-        await table.updateRow(0, createRowData(1, 4));
-        
-        const noColumns2 = Number(await table.noColumns());
-        const noRows2 = Number(await table.noRows());
-        
-        assert.equal(noColumns2, 4);
-        assert.equal(noRows2, 1);
+            const noColumns = Number(await table.noColumns());
+            const noRows = Number(await table.noRows());
+            
+            assert.equal(noColumns, 4);
+            assert.equal(noRows, 1);
+            
+            await table.updateRow(0, createRowData(1, 4));
+            
+            const noColumns2 = Number(await table.noColumns());
+            const noRows2 = Number(await table.noRows());
+            
+            assert.equal(noColumns2, 4);
+            assert.equal(noRows2, 1);
 
-        const row = await table.getRow(0);
-        
-        checkRowData(row, 1, 4);
+            const row = await table.getRow(0);
+            
+            checkRowData(row, 1, 4);
+        });
     });
 });
 
