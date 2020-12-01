d0d72f9c8ee36d27bfa4242b27cf02fc161a466c
ajlopez <webmaster@ajlopez.com>
2020-12-01 11:46:53 -0300

    TransfersTable add and get row

--- contracts/TransfersTable.sol
+++ contracts/TransfersTable.sol
@@ -8,4 +8,25 @@ contract TransfersTable {
     constructor() public {
         table = new Table(3, 1000);
     }
+    
+    function addRow(address from, address to, uint256 amount) public {
+        bytes32[] memory fields = new bytes32[](3);
+        
+        assembly {
+            mstore(add(fields, 0x20), from)
+            mstore(add(fields, 0x40), to)
+            mstore(add(fields, 0x60), amount)
+        }
+        
+        table.addRow(fields);
+    }
+    
+    function getRow(uint256 noRow) public view returns (address from, address to, uint256 amount) {
+        bytes32[] memory fields = table.getRow(noRow);
+        
+        from = address(uint256(fields[0]));
+        to = address(uint256(fields[1]));
+        amount = uint256(fields[2]);
+    }
 }
+
--- test/TransfersTable_test.js
+++ test/TransfersTable_test.js
@@ -3,6 +3,9 @@ const Table = artifacts.require('Table');
 const TransfersTable = artifacts.require('TransfersTable');
 
 contract('TransferTable', function (accounts) {
+    const alice = accounts[0];
+    const bob = accounts[1];
+    
     it('initial state', async function () {
         const transfersTable = await TransfersTable.new();
         
@@ -11,4 +14,38 @@ contract('TransferTable', function (accounts) {
         assert.equal(Number(await table.noColumns()), 3);
         assert.equal(Number(await table.noRows()), 0);
     });
+    
+    it('add row', async function () {
+        const transfersTable = await TransfersTable.new();
+        
+        const table = await Table.at(await transfersTable.table());
+        
+        await transfersTable.addRow(alice, bob, 1000);
+        
+        assert.equal(Number(await table.noColumns()), 3);
+        assert.equal(Number(await table.noRows()), 1);
+        
+        const fields = await table.getRow(0);
+        
+        assert.equal(fields[0],
+            '0x000000000000000000000000' + alice.substring(2).toLowerCase());
+        assert.equal(fields[1],
+            '0x000000000000000000000000' + bob.substring(2).toLowerCase());            
+        assert.equal(Number(fields[2]), 1000);
+    });
+    
+    it('add row and get row', async function () {
+        const transfersTable = await TransfersTable.new();
+        
+        const table = await Table.at(await transfersTable.table());
+        
+        await transfersTable.addRow(alice, bob, 1000);
+        
+        const result = await transfersTable.getRow(0);
+        
+        assert.equal(result.from, alice);
+        assert.equal(result.to, bob);
+        assert.equal(Number(result.amount), 1000);
+    });
 });
+
